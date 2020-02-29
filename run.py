55573f0b4da3aaaa1e998d592ee742e02737b7a5
Mark <m.mccormack022@gmail.com>
2020-02-29 12:39:30 +0000

    working

--- run.py
+++ run.py
@@ -15,9 +15,13 @@ class ModelThread(threading.Thread):
         self.handled = False
 
     def run(self):
-        print("Thread: Starting thread for {}".format(model["name"]))
-        time.sleep(model["time"])
-        print("Thread: Stopping thread for {}".format(model["name"]))
+        print("Thread: Starting thread for {}".format(self.name))
+        subprocess.run(["youtube-dl.exe", "-f", "best[height<=720]", "{}".format(self.url)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        # time.sleep(self.time)
+        print("Thread: Stopping thread for {}".format(self.name))
+
+    def toString(self):
+        return "{}, {}, {}".format(self.name, self.time, self.isAlive())
     
 
 if __name__ == "__main__":
@@ -26,9 +30,10 @@ if __name__ == "__main__":
         models = json.load(models_json)
     # list of model names
     model_names = [m["name"] for m in models["models"]]
-    print("NAMES: {}".format(model_names))
+    # print("NAMES: {}".format(model_names))
     
     thread_list = []
+    running_threads = {}
 
     for model in models["models"]:
         x = ModelThread(model)
@@ -48,15 +53,23 @@ if __name__ == "__main__":
                 if not t.isAlive():
                     t.handled = True
             thread_list = [t for t in thread_list if not t.handled]
-            print(thread_list)
+            # print(thread_list)
 
             # for model in models check
             # if there is a running thread that matches 
             # a models name, if not start another
-            
             active_models = [t.name for t in thread_list]
             print("active models: {}".format(active_models))
-            time.sleep(2)
+            inactive_models = [m for m in model_names if m not in active_models]
+            for model_name in inactive_models:
+                for model in models["models"]:
+                    if model["name"] == model_name:
+                        x = ModelThread(model)
+                        thread_list.append(x)
+                        x.start()
+            # print(inactive_models)
+            # print("active models: {}".format(active_models))
+            time.sleep(60)
 
     except KeyboardInterrupt:
         print("Interrupted")
